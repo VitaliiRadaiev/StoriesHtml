1a9cede956a0c8048186fe10b8e0eba1d5663f5f
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-06-14 19:45:01 +0300

    fix

--- #src/components/stories-nav/stories-nav.js
+++ #src/components/stories-nav/stories-nav.js
@@ -1,41 +1,30 @@
 {
     const storiesNavigation = document.querySelector('[data-slider="stories-nav"]');
     if (storiesNavigation) {
-        const slider = storiesNavigation.querySelector('.swiper');
-        let swiper;
 
-        function desktopSlider() {
-            if (document.documentElement.clientWidth >= 920 && storiesNavigation.dataset.mobile == 'false') {
-                swiper = new Swiper(storiesNavigation.querySelector('.swiper'), {
-                    speed: 500,
-                    watchSlidesProgress: true,
-                    watchSlidesVisibility: true,
+        const swiper = new Swiper(storiesNavigation.querySelector('.swiper'), {
+            navigation: {
+                nextEl: storiesNavigation.querySelector('.btn-left'),
+                prevEl: storiesNavigation.querySelector('.btn-right'),
+            },
+            speed: 400,
+            breakpoints: {
+                0: {
+                    slidesPerView: 'auto',
+                    spaceBetween: 0,
+                    slidesPerGroup: 2,
+                    touchRatio: 1,
+                    freeMode: true,
+                },
+                920: {
                     slidesPerView: 10,
                     spaceBetween: 20,
                     slidesPerGroup: 5,
                     touchRatio: 0,
-                    navigation: {
-                        nextEl: storiesNavigation.querySelector('.btn-left'),
-                        prevEl: storiesNavigation.querySelector('.btn-right'),
-                    }
-                });
-
-                storiesNavigation.dataset.mobile = 'true';
-            }
-
-            if (document.documentElement.clientWidth < 920) {
-                storiesNavigation.dataset.mobile = 'false';
-
-                if (slider.classList.contains('swiper-initialized')) {
-                    swiper.destroy();
+                    freeMode: false,
                 }
             }
-        }
 
-        desktopSlider();
-
-        window.addEventListener('resize', () => {
-            desktopSlider();
-        })
+        });
     }
 }
